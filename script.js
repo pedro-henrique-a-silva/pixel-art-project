b87cc23ed56ab49ea649924f24ab7d10e4cbb7d5
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-17 17:15:55 -0300

    Requisito 5 concluido, criação funções do localStorage

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
 const colorPallet = document.querySelector('#color-palette');
-
+const divColorsPaletteSelector = '#color-palette .color';
 const guide = ['#000000', '#808080', '#6A5ACD', '#000080', '#00BFFF',
-               '#008080', '#006400', '#8B4513', '#D2691E', '#4B0082',
-               '#8B008B', '#FF0000', '#FF4500', '#FFFF00', '#B0E0E6'];
-
+  '#008080', '#006400', '#8B4513', '#D2691E', '#4B0082',
+  '#8B008B', '#FF0000', '#FF4500', '#FFFF00', '#B0E0E6'];
 
 const generateRandomNumber = (number) => Math.floor(Math.random() * (number));
 
 const generateColorGuide = (amountOfColor) => {
   const arrayOfColor = ['#000000'];
-  
   while (arrayOfColor.length < amountOfColor) {
     const randomNumber = generateRandomNumber(guide.length);
-
     if (!arrayOfColor.includes(guide[randomNumber])) {
       arrayOfColor.push(guide[randomNumber]);
     }
   }
-
   return arrayOfColor;
 };
 
+const savePaletteLocalStorage = () => {
+  const colorsToSave = document.querySelectorAll(divColorsPaletteSelector);
+  const colorGuide = [];
+  for (let index = 0; index < colorsToSave.length; index += 1) {
+    colorGuide.push(colorsToSave[index].style.backgroundColor);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colorGuide));
+};
+
+const recoveryLocalStore = () => {
+  const colorGuideLocalStorage = localStorage.getItem('colorPalette');
+  if (colorGuideLocalStorage) {
+    return JSON.parse(colorGuideLocalStorage);
+  }
+
+  return generateColorGuide(4);
+};
+
+const applyColorPalette = (arrayColor) => {
+  const divColors = document.querySelectorAll(divColorsPaletteSelector);
+  for (let index = 0; index < arrayColor.length; index += 1) {
+    divColors[index].style.backgroundColor = arrayColor[index];
+  }
+};
+
 const createColorPalette = () => {
-  const colorGuide = generateColorGuide(4);
+  const colorGuide = recoveryLocalStore();
 
   for (let index = 0; index < colorGuide.length; index += 1) {
     const divColor = document.createElement('div');
     divColor.className = 'color';
-    divColor.style.backgroundColor = colorGuide[index];
     colorPallet.appendChild(divColor);
   }
 
+  applyColorPalette(colorGuide);
   const buttonRefresh = document.createElement('button');
   buttonRefresh.className = 'refresh-colors';
   buttonRefresh.id = 'button-random-color';
@@ -41,14 +62,12 @@ const createColorPalette = () => {
 const refreshColorPalette = () => {
   const buttonRefresh = document.querySelector('#button-random-color');
   buttonRefresh.addEventListener('click', () => {
-    const divColors = document.querySelectorAll('#color-palette .color')
+    const divColors = document.querySelectorAll(divColorsPaletteSelector);
     const colorGuide = generateColorGuide(divColors.length);
-
-    for (let index = 0; index < divColors.length; index += 1) {
-      divColors[index].style.backgroundColor = colorGuide[index];
-    }
-  })
-}
+    applyColorPalette(colorGuide);
+    savePaletteLocalStorage();
+  });
+};
 
 createColorPalette();
 refreshColorPalette();
